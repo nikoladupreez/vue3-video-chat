28dd7594b3ebfdcc123144cfddd2e86c19aad5f9
Nikola <nikola.du.preez@epiphany-rbc.com>
2021-11-16 11:44:13 +0100

    webrtc template

--- src/components/web-rtc.vue
+++ src/components/web-rtc.vue
@@ -1,13 +1,33 @@
 <template>
     <div class="webrtc">
+        <div class="videos-container">
+            <video
+                ref="webcamVideo"
+                class="webcam-video"
+                autoplay
+                playsinline></video>
+            <video
+                ref="remoteVideo"
+                class="remote-video"
+                autoplay
+                playsinline></video>
+        </div>
     </div>
 </template>
 
 <script>
+    import { ref } from 'vue';
+
     export default {
         name: 'WebRTC',
         async setup() {
+            // Refs
+            const webcamVideo =  ref(null);
+            const remoteVideo =  ref(null);
+
             return {
+                webcamVideo,
+                remoteVideo,
             }
         },
     }
