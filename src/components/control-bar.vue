6ffccfcfaa7126a3ee54f1e328d2dc9aec93e6b4
Nikola <nikola.du.preez@epiphany-rbc.com>
2021-11-16 11:45:08 +0100

    control bar setup

--- src/components/control-bar.vue
+++ src/components/control-bar.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="control-bar">
+        <p class="chat-id">id:</p>
+        <div class="buttons-center">
+            <button class="button-chat" @click="toggleMediaTrack">Audio</button>
+            <button class="button-chat" @click="toggleMediaTrack">Video</button>
+            <button class="button-chat leave" @click="onLeave">Leave</button>
+        </div>
+        <button class="button-chat messenger" @click="toggleMessenger">Messenger</button>
     </div>
 </template>
 
@@ -7,7 +14,23 @@
     export default {
         name: 'ControlBar',
         async setup() {
+            // Methods
+            const onLeave = () => {
+                console.log('leave');
+            };
+
+            const toggleMessenger = () => {
+                console.log('toggle messenger');
+            };
+
+            const toggleMediaTrack = () => {
+                console.log('toggle media track');
+            }
+
             return {
+                onLeave,
+                toggleMessenger,
+                toggleMediaTrack
             }
         },
     }
